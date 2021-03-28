92f28da196988463fe6aafeda09247c87290d012
Elvira Neziri <neziiri.elvira@gmail.com>
2021-03-29 01:33:51 +0200

    Added ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,60 @@
+var xy = '';
+var FcapsLock = '0';
+var Fshift = '0';
+
+function funcBackSpace() {
+    var valu = document.getElementById('display').innerHTML;
+    var x = document.getElementById('display').innerHTML.length;
+    var y = 1;
+    var z = x - y;
+    document.getElementById('display').innerHTML = valu.slice(0, z);
+}
+function funcCapsL() {
+    if (FcapsLock == '0') {
+        FcapsLock = '1';
+    } else if (FcapsLock == '1') {
+        FcapsLock = '0';
+    }
+}
+function funcShift() {
+    if (Fshift == '0') {
+        Fshift = '1';
+    } else if (Fshift == '1') {
+        Fshift = '0';
+    }
+}
+
+function funcEnter() {
+    var valu = document.getElementById('display').innerHTML;
+    document.getElementById('display').innerHTML = valu + '\n';
+}
+
+function funcSub() {
+    var valu = document.getElementById('display').innerHTML;
+    if (valu == "") {
+        alert("Please, first type some text!");
+    } else {
+        alert(valu);
+    }
+}
+
+function func(key) {
+    var valu = document.getElementById('display').innerHTML;
+    if (FcapsLock == '0') {
+        if (Fshift == '0') {
+            document.getElementById('display').innerHTML = valu + key;
+        } else if (Fshift == '1') {
+            Fshift = '0';
+            document.getElementById('display').innerHTML = valu + key.toUpperCase();
+        }
+    } else if (FcapsLock == '1') {
+        if (Fshift == '0') {
+            document.getElementById('display').innerHTML = valu + key.toUpperCase();
+        } else if (Fshift == '1') {
+            Fshift = '0';
+            document.getElementById('display').innerHTML = valu + key;
+        }
+    }
+}
+
+
